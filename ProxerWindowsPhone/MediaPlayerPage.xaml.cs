1d51c76858c1d431659a0c40bb1bf39cc5cf5c42
InfiniteSoul <infinitesoul155@gmail.com>
2016-08-22 20:07:36 +0200

    Keep the display active if a video is being watched

--- ProxerWindowsPhone/MediaPlayerPage.xaml.cs
+++ ProxerWindowsPhone/MediaPlayerPage.xaml.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using Windows.Phone.UI.Input;
+using Windows.System.Display;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace Proxer
 {
     public sealed partial class MediaPlayerPage
     {
+        private DisplayRequest _appDisplayRequest;
+
         public MediaPlayerPage()
         {
             HardwareButtons.BackPressed += this.HardwareButtonsOnBackPressed;
@@ -22,6 +26,8 @@ namespace Proxer
                 (Window.Current.Content as Frame)?.SourcePageType != typeof(MediaPlayerPage)) return;
 
             backPressedEventArgs.Handled = true;
+            this.MediaPlayer.Stop();
+
             Frame rootFrame = (Frame) Window.Current.Content;
             if (rootFrame.CanGoBack) rootFrame.GoBack();
             else rootFrame.Navigate(typeof(MainPage), null);
@@ -35,5 +41,23 @@ namespace Proxer
         }
 
         #endregion
+
+        private void MediaPlayer_CurrentStateChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.MediaPlayer.IsAudioOnly) return;
+
+            if (this.MediaPlayer.CurrentState == MediaElementState.Playing)
+            {
+                if (this._appDisplayRequest != null) return;
+                this._appDisplayRequest = new DisplayRequest();
+                this._appDisplayRequest.RequestActive();
+            }
+            else
+            {
+                if (this._appDisplayRequest == null) return;
+                this._appDisplayRequest.RequestRelease();
+                this._appDisplayRequest = null;
+            }
+        }
     }
 }
